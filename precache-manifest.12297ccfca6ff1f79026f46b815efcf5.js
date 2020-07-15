3d3ae7738919f79a8f6d2eecc58248ad12984ce7
chen-daniel <chen.daniel@husky.neu.edu>
2020-07-15 03:32:43 -0400

    Updates

---
+++ precache-manifest.12297ccfca6ff1f79026f46b815efcf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc57e44dd8e6f3ba520d26ecdcdc680",
+    "url": "/webRTC-video-chat/index.html"
+  },
+  {
+    "revision": "c2c9d1c94c3d29b5d4df",
+    "url": "/webRTC-video-chat/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2b840892f14fa97aa3ab",
+    "url": "/webRTC-video-chat/static/js/2.8bfa96d9.chunk.js"
+  },
+  {
+    "revision": "b67bc97fd0dd67004287af4028afc1b1",
+    "url": "/webRTC-video-chat/static/js/2.8bfa96d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c2c9d1c94c3d29b5d4df",
+    "url": "/webRTC-video-chat/static/js/main.5ad61c37.chunk.js"
+  },
+  {
+    "revision": "dd3671f9f44206b287cb",
+    "url": "/webRTC-video-chat/static/js/runtime-main.403e387f.js"
+  }
+]);
