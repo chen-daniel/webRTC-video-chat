6b277af36c1367418873ffe8d3f8c8502e562b62
chen-daniel <chen.daniel@husky.neu.edu>
2020-07-15 03:18:27 -0400

    Working video-chat app

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { HashRouter, Route, Switch } from "react-router-dom";
+import CreateRoom from "./routes/CreateRoom";
+import Room from "./routes/Room";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <HashRouter>
+        <Switch>
+          <Route path="/" exact component = {CreateRoom} />
+          <Route path="/room/:roomID" component = {Room} />
+        </Switch>
+      </HashRouter>
+    </div>
+  );
+}
+
+export default App;
